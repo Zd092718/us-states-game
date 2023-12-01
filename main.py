94490f4561f236641bc3e1d605f7cf18deffa280
Zack Dowd <zdowd2796@gmail.com>
2023-12-01 14:46:46 -0700

    state guessing and text positioning working

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -7,8 +7,22 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+data = pandas.read_csv("./50_states.csv")
+states = data["state"].to_list()
+guessed_states = []
 
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 Correct", prompt="What's another state's name?").title()
+
+    if answer_state in states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
 
 
 def get_mouse_click_coor(x, y):
---
+++ state.py
@@ -0,0 +1,5 @@
+from turtle import Turtle
+
+class State(Turtle):
+    def __init__(self):
+        super().__init__()
